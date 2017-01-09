61e092e1dee9b1f5f0a99b4b9524f2af5213b6cf
Miguel Martinez <mmartinez@solidemx.com>
2017-01-09 14:29:57 -0700

    email

--- sendbymail.php
+++ sendbymail.php
@@ -28,6 +28,9 @@ $headers = 'From: '.$email_from."\r\n".
 'X-Mailer: PHP/' . phpversion();
 @mail($email_to, $email_subject, $email_message, $headers);
 
-echo "Correo enviado con exito.";
+echo '<script type="text/javascript">',
+     'IsCorrect();',
+     '</script>'
+;
 }
 ?>
